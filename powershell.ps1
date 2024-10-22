1ea96def374ed1abc9e9318528294fe342a5062c
github-cloudlabsuser-1292 <168013520+github-cloudlabsuser-1292@users.noreply.github.com>
2024-10-22 05:31:57 +0000

    Copilot third commit

---
+++ powershell.ps1
@@ -0,0 +1,20 @@
+# Variables
+$resourceGroupName = "myResourceGroup"
+$location = "EastUS"
+$storageAccountName = "mystorageaccount"
+$skuName = "Standard_LRS"
+$accessTier = "Hot"
+
+# Login to Azure
+Connect-AzAccount
+
+# Create Resource Group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create Storage Account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+                     -Name $storageAccountName `
+                     -Location $location `
+                     -SkuName $skuName `
+                     -Kind StorageV2 `
+                     -AccessTier $accessTier
---
+++ terraform.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  access_tier              = var.access_tier
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  default     = "East US"
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "The tier of the storage account"
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "The replication type of the storage account"
+  type        = string
+  default     = "LRS"
+}
+
+variable "access_tier" {
+  description = "The access tier of the storage account"
+  type        = string
+  default     = "Hot"
+}
